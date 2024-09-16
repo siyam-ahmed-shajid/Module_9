e0cc43c75e5e48d2806d9582140879b51f4d4678
Siyam <siyam31121@gmail.com>
2024-09-16 23:18:35 +0600

    Module_9

--- lib/class_1.dart
+++ lib/class_1.dart
@@ -1,3 +1,5 @@
+//MediaQuery | Layout Builder | Widget Builder 
+
 import 'package:flutter/material.dart';
 import 'package:module_9/main.dart';
 
@@ -22,10 +24,69 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // MediaQuery
+    /*print(MediaQuery.of(context).size);
+    print(MediaQuery.of(context).size.width);
+    print(MediaQuery.of(context).size.height);
+    print(MediaQuery.of(context).orientation);
+    print(MediaQuery.of(context).devicePixelRatio);
+    print(MediaQuery.of(context).displayFeatures);*/
+
+    print(MediaQuery.displayFeaturesOf(context));
+    print(MediaQuery.sizeOf(context));
+
     return Scaffold(
-      appBar: AppBar (
+      appBar: AppBar(
         backgroundColor: Colors.green,
         title: const Text('Home'),
+      ),
+      /*body: Center(
+        child: Wrap(
+          alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.start,
+            spacing: 32,
+            children: [
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+              Text(MediaQuery.orientationOf(context).toString()),
+            ],
+    )*/
+
+      /*body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth < 400){
+              return const Center(child: Text('Mobile'),);
+            } else if (constraints.maxWidth < 600){
+              return const Center(child: Text('Tablet'),);
+            } else if (constraints.maxWidth < 1200){
+              return const Center(child: Text('Laptop'),);
+            }
+            return const Center(child: Text('Desktop'));
+          }
+      )*/
+
+      body: OrientationBuilder(
+        builder: (context, orientation){
+          if (orientation == Orientation.landscape){
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.yellow,
+            );
+          }
+          else{
+            return Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.red,
+            );
+          }
+        }
       ),
     );
   }
